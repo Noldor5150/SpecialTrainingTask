7c112c45351fc431f4467efedc87da99e7a39b68
Noldor5150 <ruikis.paulius@gmail.com>
2020-11-27 17:37:58 +0200

    Add project files.

---
+++ SpecialTrainingTask/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SpecialTrainingTask.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
